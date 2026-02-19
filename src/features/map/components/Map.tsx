74f971bb42fb73688bdb503e38b57e56743f525d
Juan Matias <matias@elrobacuentas.com>
2026-02-19 13:46:28 -0500

    feat: enhance map component with boundaries and refactoring

--- src/features/map/components/Map.tsx
+++ src/features/map/components/Map.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
 import { StyleSheet } from 'react-native';
 import { BurritoLocation } from '../types';
@@ -10,27 +10,40 @@ const UNMSM_LOCATION = {
   longitudeDelta: 0.012,
 };
 
+const NORTH_EAST = { latitude: -12.0520, longitude: -77.0770 };
+const SOUTH_WEST = { latitude: -12.0630, longitude: -77.0900 };
+
 interface Props {
   burritoLocation: BurritoLocation | null;
 }
 
 export const Map = ({ burritoLocation }: Props) => {
+  const mapRef = useRef<MapView>(null);
+
+  const handleMapReady = () => {
+    mapRef.current?.setMapBoundaries(NORTH_EAST, SOUTH_WEST);
+  };
+
+  console.log('mapa visto')
   return (
     <MapView
+      ref={mapRef}
       provider={PROVIDER_GOOGLE}
       style={styles.map}
       showsUserLocation={false}
       initialRegion={UNMSM_LOCATION}
+      minZoomLevel={15}
+      onMapReady={handleMapReady}
     >
       {burritoLocation && (
-        <Marker
+          <Marker
           coordinate={{
             latitude: burritoLocation.latitude,
             longitude: burritoLocation.longitude,
           }}
           rotation={burritoLocation.heading}
           title="Burrito"
-        />
+          />
       )}
     </MapView>
   );
